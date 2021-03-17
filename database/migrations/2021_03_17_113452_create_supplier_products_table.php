5f25008301c13da7db4e8ca45b44aeb78540119e
Collins Omwaga <collinsomwaga@gmail.com>
2021-03-17 14:48:39 +0300

    Initial Commit

---
+++ database/migrations/2021_03_17_113452_create_supplier_products_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSupplierProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('supplier_products', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('supply_id');
+            $table->unsignedBigInteger('product_id');
+            $table->timestamps();
+
+            $table->foreign('supply_id')
+            ->references('id')
+            ->on('suppliers')
+            ->onDelete('cascade');
+
+            $table->foreign('product_id')
+            ->references('id')
+            ->on('products')
+            ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('supplier_products');
+    }
+}
